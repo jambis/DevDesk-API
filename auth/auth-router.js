5cec63aa8ec4bac0b624eb8d5c47125d8dc5bed3
jambis <imjaamees@gmail.com>
2020-05-31 14:03:15 -0500

    Removed unused import

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const dbAuth = require("./auth-model");
 const generateToken = require("./generate-token");
